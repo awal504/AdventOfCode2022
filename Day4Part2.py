a00770f7c7cd60b5867b0b12cca7325d4a5b8373
Andrea Wallace <12562981+awal504@users.noreply.github.com>
2022-12-04 09:46:27 -0600

    Update Day4Part2.py

--- Day4Part2.py
+++ Day4Part2.py
@@ -7,10 +7,8 @@ def main():
         job = job.split(",")
         elf1 = job[0].split("-")
         elf1set = set(range(int(elf1[0]),int(elf1[1])+1))
-        print(elf1set)
         elf2 = job[1].split("-")
         elf2set = set(range(int(elf2[0]),int(elf2[1])+1))
-        print(elf2set)
         if elf1set.isdisjoint(elf2set) == False:
             running += 1
     infile.close()
