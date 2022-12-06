4f8639915f05f26529ba3515a43d80848d5a7f3f
Andrea Wallace <12562981+awal504@users.noreply.github.com>
2022-12-06 09:18:03 -0600

    Add files via upload

---
+++ Day6.py
@@ -0,0 +1,26 @@
+# Advent of Code 2022 Day 6
+def main():
+    i = 0
+    running = 0
+    search = []
+    infile = open("DaySix.txt", "r")
+    datastream = infile.read()
+    while i < len(datastream):
+        if len(search) < 4:
+            search.append(datastream[i])
+            i += 1
+        else:
+            bite = 0
+            while bite < 4:
+                running += search.count(search[bite])
+                bite += 1
+            if running > 4:
+                search.pop(0)
+            else:
+                print("Found Start at:", i)
+                break
+            running = 0
+    infile.close()
+
+
+main()
